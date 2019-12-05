c96feb8f1c3e66821b678fa0a0dd30bc093570ca
Sam Tybout <swt0007@mix.wvu.edu>
2019-12-05 15:03:14 -0500

    Added some comments to the head of get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -1,1 +1,8 @@
-wget -O "$pbdb_data.csv" "http://www.paleobiodb.org/data1.2/occs/list.csv?datainfo&rowcount&base_name=$1&taxon_reso=species&idspcmod=!ng,af,cf,sl,if,eg,qm,qu&max_ma=250&min_ma=0&show=ident,class,refattr,acconly"
+# Downloads data from the Paleobiology Database for the requested taxonomic group.
+
+# Usage: bash get_data Taxon_name
+# Products: pbdb_data.csv, a csv file containing occurences in the Paleobiology Database of the requested group.
+# For each variable, the file contains data on its taxonmic identification,
+# its geologic age, and the publication in which the occurence is described.
+
+wget -O "pbdb_data.csv" "http://www.paleobiodb.org/data1.2/occs/list.csv?datainfo&rowcount&base_name=$1&taxon_reso=species&idspcmod=!ng,af,cf,sl,if,eg,qm,qu&show=ident,class,refattr,acconly"
