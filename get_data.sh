0c1ece86946a8f19379849f9d0ec1eb07c919ea8
Sam Tybout <swt0007@mix.wvu.edu>
2019-12-16 13:24:42 -0500

    Expanded readme, simplified get_data.sh

--- get_data.sh
+++ get_data.sh
@@ -5,4 +5,4 @@
 # For each variable, the file contains data on its taxonmic identification,
 # its geologic age, and the publication in which the occurence is described.
 
-wget -O "pbdb_data.csv" "http://www.paleobiodb.org/data1.2/occs/list.csv?datainfo&rowcount&base_name=$1&taxon_reso=species&idspcmod=!ng,af,cf,sl,if,eg,qm,qu&show=ident,class,refattr,acconly"
+wget -O "pbdb_data.csv" "http://www.paleobiodb.org/data1.2/occs/list.csv?&base_name=$1&taxon_reso=species&idspcmod=!ng,af,cf,sl,if,eg,qm,qu&show=ident,class,refattr,acconly"
